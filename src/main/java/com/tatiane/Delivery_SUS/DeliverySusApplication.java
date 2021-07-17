c7914a43ebd7cc3cfe1180faedf5906e79516fad
Tatiane Anjos <tatianeanjos1997@gmail.com>
2021-07-17 16:31:15 -0300

    Projeto criado

---
+++ src/main/java/com/tatiane/Delivery_SUS/DeliverySusApplication.java
@@ -0,0 +1,13 @@
+package com.tatiane.Delivery_SUS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverySusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverySusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tatiane/Delivery_SUS/DeliverySusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatiane.Delivery_SUS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverySusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
