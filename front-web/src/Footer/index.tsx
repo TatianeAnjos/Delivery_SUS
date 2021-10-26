8238956260b71eb478e4fee16262ba2dede84eda
elyl-lucas <63608302+elyl-lucas@users.noreply.github.com>
2021-10-26 17:37:36 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+
+function Footer (){
+    return(
+        <footer className="main-footer">
+            App desenvolvido para TCC UNIP.
+           
+        </footer>
+    );
+
+}
+
+export default Footer;
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,24 +1,30 @@
-import './styles.css';
+import Footer from "../Footer";
+import "./styles.css";
+import { ReactComponent }  from './logo.svg';
 
-function Home(){
-    return (
-       <div className="home-container">
-           <div className="home-content">
-                <div className="home-actions">
-                    <h1 className="home-title">
-                        Agora o Sus <br />
-                        leva o remedio <br />
-                        até você!!!
-                    </h1>
-                        <a className="home-btn-order">
-                            Entrar <br />
-                        </a>
-                        
-                </div>
-           </div>
-       </div>
-    );
-
+function Home() {
+  return (
+    <>
+      <div className="home-container">
+        <div className="home-content">
+          <div className="home-actions">
+            <h1 className="home-title">
+              Agora o Sus <br />
+              leva o remedio <br />
+              até você!!!
+            </h1>
+            <a className="home-btn-order">
+              Entrar <br />
+            </a>
+          </div>
+          <div className="home-image">
+              <ReactComponent />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
