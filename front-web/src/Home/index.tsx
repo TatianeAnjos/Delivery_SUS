b9a4eadca8b10c6e8edd330cbab79ef362e29ff9
elyl-lucas <63608302+elyl-lucas@users.noreply.github.com>
2021-10-26 16:17:50 -0300

    Add Home Page

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,24 @@
+import './styles.css';
+
+function Home(){
+    return (
+       <div className="home-container">
+           <div className="home-content">
+                <div className="home-actions">
+                    <h1 className="home-title">
+                        Agora o Sus <br />
+                        leva o remedio <br />
+                        até você!!!
+                    </h1>
+                        <a className="home-btn-order">
+                            Entrar <br />
+                        </a>
+                        
+                </div>
+           </div>
+       </div>
+    );
+
+}
+
+export default Home;
